ba66ef24171458a3bb35127c1b6deb454df898e3
ELCoche187 <mikaedling@gmail.com>
2023-11-18 21:42:51 +0100

    creating a json to send to backend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,8 +7,8 @@ import { HomeViewComponent} from "./views/home-view/home-view.component";
 import { ProfileViewComponent } from './views/profile-view/profile-view.component';
 import { IonicModule } from '@ionic/angular';
 import { LoginComponent } from './components/login/login.component';
-
-
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 
 const routes: Routes = [
@@ -25,12 +25,13 @@ const routes: Routes = [
     HomeViewComponent,
     ProfileViewComponent,
     LoginComponent,
-
   ],
   imports: [
     RouterModule.forRoot(routes),
     IonicModule.forRoot(),
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
+
 export class LoginComponent {
+  constructor(private http : HttpClient) {
+  }
+  loginModel: any = {};
+
+  onSubmit(){
+    const backendUrl = "" ;
+
+    const jsonString = JSON.stringify(this.loginModel);
+
+    this.http.post(backendUrl, jsonString)
+      .subscribe(response =>
+      {
+        console.log(response);
+      }, error => {
+        console.error(error)
+      });
+
+
+  }
+
 
 }
+
