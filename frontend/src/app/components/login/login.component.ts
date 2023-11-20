7fd13fe726eb6dc729c8d23a8731b849e4fde08e
Jklaric <klaric9jakov@gmail.com>
2023-11-20 14:56:50 +0100

    fix bug with login button not issuing an http request

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,34 +1,23 @@
 import { Component } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-
-
 export class LoginComponent {
-  constructor(private http : HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
   loginModel: any = {};
 
-  onSubmit(){
-    const backendUrl = "" ;
-
+  login() {
+    const backendUrl = 'http://localhost:8080/api/login';
     const jsonString = JSON.stringify(this.loginModel);
 
-    this.http.post(backendUrl, jsonString)
-      .subscribe(response =>
-      {
-        console.log(response);
-      }, error => {
-        console.error(error)
-      });
-
-
+    this.http.post(backendUrl, jsonString).subscribe((response: any) => {
+      console.log(response);
+      console.log('Button works!');
+    });
   }
-
-
 }
-
