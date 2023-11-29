8d8dc8faccd76da1d87adbca1741cf90a4aa4f4d
Lordstacker <nicoenrique_1994@hotmail.com>
2023-11-29 23:25:46 +0100

    login phase to modal modified

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import {ModalController} from "@ionic/angular";
+
 
 @Component({
   selector: 'app-login',
@@ -12,7 +13,10 @@ export class LoginComponent {
   constructor(
     private http: HttpClient,
     private router: Router,
-  ) {}
+    private modalController: ModalController
+  ) {
+
+  }
 
   loginModel: any = {};
   backendUrl = 'http://localhost:5000/api/account/login';
@@ -25,13 +29,19 @@ export class LoginComponent {
 
     this.http.post(this.backendUrl, formData).subscribe(
       (response: any) => {
-        console.log(response);
+        localStorage.setItem('isLoggedIn', 'true');
+        localStorage.setItem('token', response.token);
         this.router.navigate(['/']);
+        this.closeModal();
       },
       (error) => {
         console.error('An error occurred:', error);
       },
     );
     this.loginModel = [];
+  }
+
+  closeModal() {
+    this.modalController.dismiss();
   }
 }
--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, NgZone } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
+
 export class NavbarComponent {
+  isLoggedIn = false;
+
+  constructor(
+    private modalController: ModalController,
+    private changeDetectorRef: ChangeDetectorRef,
+    private ngZone: NgZone
+  ) {
+    const isLoggedIn = localStorage.getItem('isLoggedIn');
+    this.isLoggedIn = isLoggedIn === 'true';
+  }
+
+  async toggleLoginStatus() {
+    if (this.isLoggedIn) {
+      localStorage.setItem('isLoggedIn', 'false');
+      localStorage.removeItem('token');
+      this.isLoggedIn = false;
+    } else {
+      // Logging in
+      const modal = await this.modalController.create({
+        component: LoginComponent,
+      });
+      await modal.present();
+    }
+    this.updateLoginStatus();
+  }
+  ngDoCheck() {
+    // Trigger change detection whenever ngDoCheck is called
+    this.changeDetectorRef.detectChanges();
+  }
+
+  private updateLoginStatus() {
+    const isLoggedIn = localStorage.getItem('isLoggedIn');
+    this.isLoggedIn = isLoggedIn === 'true';
+  }
 
 }
